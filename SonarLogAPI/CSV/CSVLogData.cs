09a4dab98030377ca7f6bc11618b061c970bb3bb
risty <ivan@bagrich.ru>
2017-11-28 16:18:31 +0300

    CSV Data point type and small fixes

--- SonarLogAPI/CSV/CSVLogData.cs
+++ SonarLogAPI/CSV/CSVLogData.cs
@@ -4,31 +4,33 @@
 	using System.Collections.Generic;
 	using System.IO;
 
+	using SonarLogAPI.Primitives;
+
 	/// <summary>
 	///Log data that consists of CSV entries.
 	/// </summary>
 	public class CsvLogData
 	{
 		/// <summary>
-		/// Data Name
+		/// Data Name.
 		/// </summary>
 		public string Name { get; set; }
 
 		/// <summary>
-		/// Data creation time
+		/// Data creation time.
 		/// </summary>
 		public DateTimeOffset CreationDateTime { get; set; }
 
 		/// <summary>
-		/// Depth points
+		/// Depth points.
 		/// </summary>
-		public List<CsvLogEntry> Points { get; set; }
+		public IEnumerable<IDepthPointSource> Points { get; set; }
 
 		/// <summary>
-		/// Writes <see cref="CsvLogData"/> object to <see cref="Stream"/>
+		/// Writes <see cref="CsvLogData"/> object to <see cref="Stream"/>.
 		/// </summary>
-		/// <param name="stream"><see cref="Stream"/></param>
-		/// <param name="data"><see cref="CsvLogData"/> object</param>
+		/// <param name="stream"><see cref="Stream"/>.</param>
+		/// <param name="data"><see cref="CsvLogData"/> object.</param>
 		public static void WriteToStream(Stream stream, CsvLogData data)
 		{
 			using (var writer = new StreamWriter(stream))
