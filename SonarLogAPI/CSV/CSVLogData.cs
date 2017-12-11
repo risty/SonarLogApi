38159732cc8950d73ef962a2d35c6c0775986c51
risty <ivan@bagrich.ru>
2017-12-11 15:24:51 +0300

    CSVLogData read from stream

--- SonarLogAPI/CSV/CSVLogData.cs
+++ SonarLogAPI/CSV/CSVLogData.cs
@@ -3,6 +3,8 @@
 	using System;
 	using System.Collections.Generic;
 	using System.IO;
+	using System.Linq;
+	using System.Text;
 
 	using SonarLogAPI.Primitives;
 
@@ -33,13 +35,46 @@
 		/// <param name="data"><see cref="CsvLogData"/> object.</param>
 		public static void WriteToStream(Stream stream, CsvLogData data)
 		{
-			using (var writer = new StreamWriter(stream))
+			if (!stream.CanWrite)
+				throw new InvalidOperationException("Can't write to stream");
+
+			using (var writer = new StreamWriter(stream, Encoding.UTF8))
 			{
 				foreach (var point in data.Points)
 				{
 					writer.WriteLine(point);
 				}
 			}
+		}
+
+		/// <summary>
+		/// Reads <see cref="CsvLogData"/> from <see cref="Stream"/>.
+		/// </summary>
+		/// <param name="stream"><see cref="Stream"/>.</param>
+		/// <returns><see cref="CsvLogData"/> object.</returns>
+		public static CsvLogData ReadFromStream(Stream stream)
+		{
+			if (!stream.CanRead)
+				throw new InvalidOperationException("Can't read from stream");
+
+			var listofEntrys = new List<CsvLogEntry>();
+			var valuesOrder = new Dictionary<int, string> { { 0, "Latitude" }, { 1, "Longitude" }, { 2, "Depth" } };
+
+			using (var reader = new StreamReader(stream, Encoding.UTF8))
+			{
+				while (!reader.EndOfStream)
+				{
+					if (CsvLogEntry.TryParse(reader.ReadLine(), ',', LinearDimensionUnit.Meter, valuesOrder, out var result))
+					listofEntrys.Add(result);
+				}
+			}
+
+			return new CsvLogData()
+			{
+				CreationDateTime = DateTimeOffset.Now,
+				Points = listofEntrys,
+				Name = $"{listofEntrys.Count} points with depths from {listofEntrys.Select(entry=> entry.Depth.GetMeters()).Min()} to {listofEntrys.Select(entry => entry.Depth.GetMeters()).Max()}."
+			};
 		}
 	}
 }
