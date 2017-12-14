7d46a3b084ade0df760aed78d1fc5105214083be
risty <ivan@bagrich.ru>
2017-12-14 17:27:17 +0300

    Compass entry, data and tests

---
+++ SonarLogAPI/Compass/CompassLogData.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SonarLogAPI.Compass
+{
+
+	/// <summary>
+	///Log data that consists of <see cref="CompassLogEntry"/>s.
+	/// </summary>
+	public class CompassLogData
+	{
+		/// <summary>
+		/// Data Name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Data creation time.
+		/// </summary>
+		public DateTimeOffset CreationDateTime { get; set; }
+
+		/// <summary>
+		/// <see cref="CompassLogEntry"/>s.
+		/// </summary>
+		public IEnumerable<CompassLogEntry> CompassLogEntrys { get; set; }
+
+		/// <summary>
+		/// Writes <see cref="CompassLogData"/> object to <see cref="Stream"/>.
+		/// </summary>
+		/// <param name="stream"><see cref="Stream"/>.</param>
+		/// <param name="data"><see cref="CompassLogData"/></param>
+		public static void WriteToStream(Stream stream, CompassLogData data)
+		{
+			if (!stream.CanWrite)
+				throw new InvalidOperationException("Can't write to stream");
+
+			using (var writer = new StreamWriter(stream, Encoding.UTF8))
+			{
+				foreach (var entry in data.CompassLogEntrys)
+				{
+					writer.WriteLine(entry);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Reads <see cref="CompassLogData"/> from <see cref="Stream"/> with default values order. Default order is { 0, "Time" }, { 1, "Latitude" }, { 2, "Longitude" }, { 3, "Heading" }, { 4, "Pitch" }, { 5, "Roll" }.
+		/// </summary>
+		/// <param name="stream"><see cref="Stream"/>.</param>
+		/// <returns><see cref="CompassLogData"/> object.</returns>
+		public static CompassLogData ReadFromStream(Stream stream)
+		{
+			return ReadFromStream(stream, new Dictionary<int, string>() { { 0, "Time" }, { 1, "Latitude" },
+				{ 2, "Longitude" }, { 3, "Heading" }, { 4, "Pitch" }, { 5, "Roll" } });
+		}
+
+		/// <summary>
+		/// Reads <see cref="CompassLogData"/> from <see cref="Stream"/> with specified values order.
+		/// </summary>
+		/// <param name="stream"><see cref="Stream"/>.</param>
+		/// <param name="valuesOrder">Order of values at strings. For example: { 0, "Time" }, { 1, "Latitude" }, { 2, "Longitude" }, { 3, "Heading" }, { 4, "Pitch" }, { 5, "Roll" }.</param>
+		/// <returns><see cref="CompassLogData"/> object.</returns>
+		public static CompassLogData ReadFromStream(Stream stream, IDictionary<int, string> valuesOrder)
+		{
+			if (!stream.CanRead)
+				throw new InvalidOperationException("Can't read from stream");
+
+			var listofEntrys = new List<CompassLogEntry>();
+
+			using (var reader = new StreamReader(stream, Encoding.UTF8))
+			{
+				while (!reader.EndOfStream)
+				{
+					if (CompassLogEntry.TryParse(reader.ReadLine(), ',', valuesOrder, out var result))
+						listofEntrys.Add(result);
+				}
+			}
+
+			return new CompassLogData()
+			{
+				CreationDateTime = DateTimeOffset.Now,
+				CompassLogEntrys = listofEntrys,
+				Name = $"{listofEntrys.Count} compass entrys."
+			};
+		}
+	}
+}
--- SonarLogAPI/Compass/CompassLogEntry.cs
+++ SonarLogAPI/Compass/CompassLogEntry.cs
@@ -1,36 +1,217 @@
 ﻿namespace SonarLogAPI.Compass
 {
 	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+
 	using Primitives;
 
-	/// <inheritdoc />
 	/// <summary>
 	/// The log entry with information from compass.
 	/// </summary>
-	/// <seealso cref="!:http://en.wikipedia.org/wiki/Flight_dynamics_(fixed-wing_aircraft)" />
-	public class CompassLogEntry : ICoordinatePointSource
+	/// <seealso cref="http://en.wikipedia.org/wiki/Flight_dynamics_(fixed-wing_aircraft)" />
+	public class CompassLogEntry : ICoordinatePointSource, IEquatable<CompassLogEntry>
 	{
 		/// <summary>
-		/// Entry DateTimeOffset.
+		/// Entry EntryDateTimeOffset.
 		/// </summary>
-		public DateTimeOffset DateTimeOffset { get; set; }
+		public DateTimeOffset EntryDateTimeOffset { get; set; }
 
 		/// <inheritdoc />
 		public CoordinatePoint Point { get; set; }
 
 		/// <summary>
-		/// Heading (magnetic azimuth).
+		/// Heading (magnetic azimuth), degrees.
 		/// </summary>
 		public double Heading { get; set; }
 
 		/// <summary>
-		/// Pitch in radians. "-" front side turn and "+" back side turn.
+		/// Pitch in radians. "-" front side turn and "+" back side turn, degrees.
 		/// </summary>
 		public double Pitch { get; set; }
 
 		/// <summary>
-		/// Roll in radians. "+" for right side turn and "-" left side turn.
+		/// Roll in radians. "+" for right side turn and "-" left side turn, degrees.
 		/// </summary>
 		public double Roll { get; set; }
+
+		/// <summary>
+		/// Create instance of <see cref="CompassLogEntry"/> with <see cref="EntryDateTimeOffset"/> and MRU data.
+		/// </summary>
+		/// <param name="entryDateTimeOffset"></param>
+		/// <param name="heading"><see cref="Heading"/></param>
+		/// <param name="pitch"><see cref="Pitch"/></param>
+		/// <param name="roll"><see cref="Roll"/></param>
+		public CompassLogEntry(DateTimeOffset entryDateTimeOffset, double heading, double pitch, double roll)
+		{
+			EntryDateTimeOffset = entryDateTimeOffset;
+			Heading = heading;
+			Pitch = pitch;
+			Roll = roll;
+		}
+
+		/// <summary>
+		/// Create instance of <see cref="CompassLogEntry"/> with <see cref="CoordinatePoint"/> and MRU data.
+		/// </summary>
+		/// <param name="point"><see cref="CoordinatePoint"/></param>
+		/// <param name="heading"><see cref="Heading"/></param>
+		/// <param name="pitch"><see cref="Pitch"/></param>
+		/// <param name="roll"><see cref="Roll"/></param>
+		public CompassLogEntry(CoordinatePoint point, double heading, double pitch, double roll)
+		{
+			Point = point;
+			Heading = heading;
+			Pitch = pitch;
+			Roll = roll;
+		}
+
+		public static bool TryParse(string cvsLogEntryString, char charForSplit, IDictionary<int, string> valuesOrder, out CompassLogEntry result)
+		{
+			result = null;
+
+			long dateParseResult = 0;
+			Latitude lat = null;
+			Longitude lon = null;
+			double heading = 0;
+			double pitch = 0;
+			double roll = 0;
+			var values = cvsLogEntryString.Split(charForSplit);
+
+			// if string does't contanes values or values count less then expect return false
+			if (values.Length == 0 || values.Length < valuesOrder.Count)
+				return false;
+
+			for (var i = 0; i < values.Length; i++)
+			{
+				if (valuesOrder.ContainsKey(i))
+				{
+					if (CultureInfo.InvariantCulture.CompareInfo.IndexOf(valuesOrder[i],
+							"Time", CompareOptions.IgnoreCase) >= 0)
+					{
+						var parceresult = long.TryParse(values[i], NumberStyles.Any, CultureInfo.InvariantCulture,
+							out dateParseResult);
+
+						if (parceresult)
+							continue;
+						return false;
+					}
+
+					if (CultureInfo.InvariantCulture.CompareInfo.IndexOf(valuesOrder[i],
+							"Latitude", CompareOptions.IgnoreCase) >= 0)
+					{
+						var parceresult = Latitude.TryParse(values[i], out lat);
+
+						if (parceresult)
+							continue;
+						return false;
+					}
+
+					if (CultureInfo.InvariantCulture.CompareInfo.IndexOf(valuesOrder[i],
+							"Longitude", CompareOptions.IgnoreCase) >= 0)
+					{
+						var parceresult = Longitude.TryParse(values[i], out lon);
+
+						if (parceresult)
+							continue;
+						return false;
+					}
+
+					if (CultureInfo.InvariantCulture.CompareInfo.IndexOf(valuesOrder[i],
+							"Heading", CompareOptions.IgnoreCase) >= 0)
+					{
+						var parceresult = double.TryParse(values[i], NumberStyles.Any, CultureInfo.InvariantCulture,
+							out heading);
+
+						if (parceresult)
+							continue;
+						return false;
+					}
+
+					if (CultureInfo.InvariantCulture.CompareInfo.IndexOf(valuesOrder[i],
+							"Pitch", CompareOptions.IgnoreCase) >= 0)
+					{
+						var parceresult = double.TryParse(values[i], NumberStyles.Any, CultureInfo.InvariantCulture,
+							out pitch);
+
+						if (parceresult)
+							continue;
+						return false;
+					}
+
+					if (CultureInfo.InvariantCulture.CompareInfo.IndexOf(valuesOrder[i],
+							"Roll", CompareOptions.IgnoreCase) >= 0)
+					{
+						var parceresult = double.TryParse(values[i], NumberStyles.Any, CultureInfo.InvariantCulture,
+							out roll);
+
+						if (parceresult)
+							continue;
+						return false;
+					}
+				}
+
+			}
+
+			if (lat != null && lon != null)
+			{
+				result = new CompassLogEntry(new CoordinatePoint(lat, lon), heading, pitch, roll);
+				if (dateParseResult != 0)
+					result.EntryDateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(dateParseResult);
+
+				return true;
+			}
+
+			if (dateParseResult != 0)
+			{
+				result = new CompassLogEntry(DateTimeOffset.FromUnixTimeMilliseconds(dateParseResult), heading, pitch, roll);
+				return true;
+			}
+
+			return false;
+		}
+
+
+		public override string ToString()
+		{
+			return string.Format(CultureInfo.InvariantCulture, "{0},{1},{2},{3},{4},{5}",
+				EntryDateTimeOffset.ToUnixTimeMilliseconds(), Point?.Latitude.ToDegrees(), Point?.Longitude.ToDegrees(), Heading, Pitch, Roll);
+		}
+
+		public bool Equals(CompassLogEntry other)
+		{
+			if (ReferenceEquals(null, other))
+				return false;
+			if (ReferenceEquals(this, other))
+				return true;
+			return EntryDateTimeOffset.Equals(other.EntryDateTimeOffset) 
+				&& Equals(Point, other.Point) 
+				&& Heading.Equals(other.Heading) 
+				&& Pitch.Equals(other.Pitch) 
+				&& Roll.Equals(other.Roll);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj))
+				return false;
+			if (ReferenceEquals(this, obj))
+				return true;
+			if (obj.GetType() != this.GetType())
+				return false;
+			return Equals((CompassLogEntry)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hashCode = EntryDateTimeOffset.GetHashCode();
+				hashCode = (hashCode * 397) ^ (Point != null ? Point.GetHashCode() : 0);
+				hashCode = (hashCode * 397) ^ Heading.GetHashCode();
+				hashCode = (hashCode * 397) ^ Pitch.GetHashCode();
+				hashCode = (hashCode * 397) ^ Roll.GetHashCode();
+				return hashCode;
+			}
+		}
 	}
 }
